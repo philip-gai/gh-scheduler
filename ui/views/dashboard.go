223a433ac8b15558d7280298d2e2d6a474ba6c32
Philip Gai <philip-gai@github.com>
2022-07-26 20:23:15 +0000

    Add helpful links

--- ui/views/dashboard.go
+++ ui/views/dashboard.go
@@ -7,6 +7,10 @@ import (
 	textinput "github.com/philip-gai/gh-scheduler/ui/components"
 )
 
+// https://github.com/charmbracelet/bubbletea/tree/master/tutorials/basics
+// https://github.com/charmbracelet/bubbles
+// https://www.inngest.com/blog/interactive-clis-with-bubbletea
+
 func Init() {
 	p := tea.NewProgram(textinput.InitialModel())
 
